9abf1af3b9c0b1b758f472da2ff1052e79649a16
Roman Birg <roman@cyngn.com>
2014-12-05 15:54:34 +0000

    Utils: add isPackageInstalled boolean

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1012,4 +1012,20 @@ public final class Utils {
         }
         return sb.toString();
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        if (pkg == null) {
+            return false;
+        }
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+            if (!pi.applicationInfo.enabled) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
 }

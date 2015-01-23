16aea982e3422941c3a28d8e3898423bcb2f846d
LorDClockaN <davor@losinj.com>
2015-01-23 15:48:57 +0100

    Clean up code after ScreenType FWB commit
    Change-Id: I07874c68802674695877c6bd0329c8973dd1c653

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -69,6 +69,8 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.search.SearchIndexableRaw;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.cyanogenmod.SystemSettingSwitchPreference;
 
 import java.text.Collator;
@@ -220,7 +222,7 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
 
         mFullScreenKeyboard = (
                 SystemSettingSwitchPreference) findPreference(KEY_FULL_SCREEN_KEYBOARD);
-        if (mFullScreenKeyboard != null && !Utils.isPhone(getActivity())) {
+        if (mFullScreenKeyboard != null && !ScreenType.isPhone(getActivity())) {
             getPreferenceScreen().removePreference(mFullScreenKeyboard);
         }
 
